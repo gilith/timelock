34e4378acdaaf031e101945d02b818d5d0b154d1
Joe Leslie-Hurd <joe@gilith.com>
2015-06-07 14:18:44 -0700

    main function

--- driver/Test.hs
+++ driver/Test.hs
@@ -1,8 +1,11 @@
 import qualified Data.ByteString.Char8 as B
 import System.Hardware.Serialport
-let port = "COM3"          -- Windows
---let port = "/dev/ttyUSB0"  -- Linux
-s <- openSerial port defaultSerialSettings { commSpeed = CS2400 }
-send s $ B.singleton 15
-recv s 1 >>= print
-closeSerial s
+
+main :: IO ()
+main =
+    do let port = "COM3"          -- Windows
+       --let port = "/dev/ttyUSB0"  -- Linux
+       s <- openSerial port defaultSerialSettings { commSpeed = CS2400 }
+       send s $ B.singleton 15
+       recv s 1 >>= print
+       closeSerial s
